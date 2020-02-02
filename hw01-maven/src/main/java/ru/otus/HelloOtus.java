e91c285d50279365fda341180008919b9fa7beda
ribalca <ribalca@mail.ru>
2020-02-03 00:54:09 +0300

    new file:   hw01-maven/pom.xml 	new file:   hw01-maven/src/main/java/ru/otus/HelloOtus.java 	new file:   pom.xml 	modified:   hw01-maven/src/main/java/ru/otus/HelloOtus.java

---
+++ hw01-maven/src/main/java/ru/otus/HelloOtus.java
@@ -0,0 +1,40 @@
+package ru.otus;
+
+import com.google.common.collect.Lists;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * java -cp hw01-maven-jar-with-dependencies.jar ru.otus.HelloOtus
+ * 
+ */
+
+public class HelloOtus {
+
+    private static final int MEASURE_COUNT = 1;
+    public static void main(String... args) {
+        List<Integer> example = new ArrayList<>();
+        int min = 0;
+        int max = 15;
+        for (int i = min; i < max + 1; i++) {
+            example.add(i);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        Collections.shuffle(example);
+        calcTime(() -> result.addAll(Lists.reverse(example)));
+        System.out.println(result);
+    }
+
+    private static void calcTime(Runnable runnable) {
+        long startTime = System.nanoTime();
+        for (int i = 0; i < MEASURE_COUNT; i++) {
+            runnable.run();
+        }
+        long finishTime = System.nanoTime();
+        long timeNs = (finishTime - startTime) / MEASURE_COUNT+1;
+        System.out.println("Time spent: " + timeNs + "ns (" + timeNs / 1_000 + "ms)");
+    }
+}
